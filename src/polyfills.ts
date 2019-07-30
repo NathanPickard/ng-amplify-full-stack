986345a8226ca19753af334676ad03bae84a4b03
Nathan Pickard <nathan.pickard87@gmail.com>
2019-07-30 13:47:38 -0700

    Project scaffolding

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,6 +18,11 @@
  * BROWSER POLYFILLS
  */
 
+ declare global {
+   interface Window { global: any; }
+ }
+ window.global = window;
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
